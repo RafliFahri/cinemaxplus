b008cb2f225d040489eb7461493abc676d0b566d
Rafli Fahri <raflianang73@gmail.com>
2022-12-12 22:25:03 +0700

    Update Link & Script Tag

--- index.php
+++ index.php
@@ -4,15 +4,25 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>CineMax Plus</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Zen+Dots&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/css/master.csshttps://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+    <style>
+      @import url('https://fonts.googleapis.com/css2?family=Zen+Dots&display=swap');
+      font-family: 'Zen Dots', cursive;
+    </style>
   </head>
   <body>
     <nav>
       <div class="logo">
-
+        <img src="" alt="">
       </div>
     </nav>
     <main>
 
     </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" charset="utf-8"></script>
   </body>
 </html>
